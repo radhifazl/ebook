c02d27ca6114f4df89da88db36633b3081c04d19
Radhif Azli <77906566+radhifazli@users.noreply.github.com>
2021-11-19 17:43:30 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+const navMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            toggle.classList.toggle('nav-open');
+            nav.classList.toggle('nav-open');
+        });
+    };
+};
+
+navMenu('hamburger-menu', 'nav-menu');
+
+const navLinks = document.querySelectorAll('.nav-link');
+function linkAction() {
+    navLinks.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+}
+navLinks.forEach(n => n.addEventListener('click', linkAction));
+
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('nav-scrolled', window.scrollY > 10);
+});
+
+const buyBtn = document.querySelector('.buy-btn');
+const buyIcon = document.getElementById('cart-icon');
+buyBtn.addEventListener('mouseenter', () => {
+    buyIcon.classList.add('bx-tada');
+});
+
+buyBtn.addEventListener('mouseleave', () => {
+    buyIcon.classList.remove('bx-tada');
+});
+
+const chatBox = document.getElementById('chat');
+
+const chatWhatsapp = () => {
+    var chatBtn = document.querySelector('.submit-btn');
+    chatBtn.addEventListener('click', () => {
+        var inputWa = chatBox.value;
+        var waText = inputWa.split(" ").join("%20");
+        window.open(`https://wa.me/6283819801472/?text=${waText}`);
+    });
+};
+
+chatWhatsapp();
+
+
